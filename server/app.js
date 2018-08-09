6891fe917240be9ff7f3b152df5fceda689c83e1
hungkm <hungkm@fsoft.com.vn>
2018-08-09 15:56:52 +0700

    Using mongoose to create message schema and create list messages API

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var messagesRouter = require('./routes/messages');
 
 var app = express();
 
@@ -17,8 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
+app.use('/messages', messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
